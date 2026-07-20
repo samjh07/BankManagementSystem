c11ef24b04422180d5c281c4132eac7cd5c89843
samjh07 <samridhjha17@gmail.com>
2026-07-20 23:53:50 +0530

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,20 @@
 package main;
+import exception.AccountNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+
+            throw new AccountNotFoundException(
+                    "Account not found!");
+
+        }
+        catch (AccountNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
 
         Menu menu = new Menu();
         menu.start();
